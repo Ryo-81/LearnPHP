a5f8b90e0c342b17c75b08470edea22d3ac17dc7
Ryo-81 <learningthephp@gmail.com>
2015-06-08 04:32:12 +0900

    remake

--- main.php
+++ main.php
@@ -1,7 +1,6 @@
 <?php
-include_once './Classes/Counter/Counter.php';
-include_once './Classes/Counter/ImageCounter.php';
+$str_ClassPath = dirname(__FILE__) . '/Classes/Counter/ImageCounter.php';
+include_once $str_ClassPath;
 
 $c = new ImageCounter();
-// echo $c -> Get_int_Count();
-$c -> Get_HTML_CountImage();
+echo $c -> Get_str_CountImage(8);
