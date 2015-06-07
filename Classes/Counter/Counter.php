9a2dac1253aa418493b8c34fd73338664d54c254
Ryo-81 <learningthephp@gmail.com>
2015-06-08 04:35:38 +0900

    Revert "Revert "remake""
    This reverts commit c0b62283f205576043d81aa7ee3b443d198e26da.

--- Classes/Counter/Counter.php
+++ Classes/Counter/Counter.php
@@ -1,39 +1,47 @@
 <?php
+/**
+ * アクセスカウンタ<br>
+ * インスタンス時にカウントアップ
+ * @author VM
+ */
 class Counter {
+	// カウントデータファイル名
 	private $str_fileName = "_A_COUNT.txt";
-	protected $str_fileDir;
+	// アクセスカウント
 	protected $int_count;
+	
 	/**
-	 * @アクセスカウンタ
-	 * @インスタンス時にカウントアップ
-	 * @
-	 * @コンストラクタ
-	 * @void __construct($str_fileDir = "./")
-	 * @ String $str_fileDir カウンタファイルのあるディレクトリ、デフォルトはルートディレクトリ
-	 * @
-	 * @カウント数を取得
-	 * @int Get_int_Count()
+	 * コンストラクタ<br>&emsp
+	 * void __construct($str_fileDir = "./")
+	 *
+	 * @param String $str_fileDir
+	 *        	カウンタファイルのあるディレクトリ、デフォルトはルートディレクトリ
 	 */
 	function __construct($str_fileDir = "./") {
-		$this -> str_fileDir = rtrim($str_fileDir, "/") . "/";
-		if(! file_exists($this -> str_fileDir)){
-			die("not found directry.<br>\n" . $this -> str_fileDir);
+		$str_fileDir = rtrim($str_fileDir, "/") . "/";
+		if(! file_exists($str_fileDir)){
+			die("not found directry.<br>\n" . $str_fileDir);
 		}
 		try{
-			if(! file_exists($this -> str_fileDir . $this -> str_fileName)){
-				if(false === @file_put_contents($this -> str_fileDir . $this -> str_fileName, "0")){
-					die("failed make file.<br>\n" . $this -> str_fileDir . $this -> str_fileName);
+			if(! file_exists($str_fileDir . $this -> str_fileName)){
+				if(false === @file_put_contents($str_fileDir . $this -> str_fileName, "0")){
+					die("failed make file.<br>\n" . $str_fileDir . $this -> str_fileName);
 				}
 			}
-			$this -> int_count = (intval(file_get_contents($this -> str_fileDir . $this -> str_fileName)));
+			$this -> int_count = (intval(file_get_contents($str_fileDir . $this -> str_fileName)));
 			$this -> int_count++;
-			if(false === @file_put_contents($this -> str_fileDir . $this -> str_fileName, $this -> int_count)){
-				die("failed update file.<br>\n" . $this -> str_fileDir . $this -> str_fileName);
+			if(false === @file_put_contents($str_fileDir . $this -> str_fileName, $this -> int_count)){
+				die("failed update file.<br>\n" . $str_fileDir . $this -> str_fileName);
 			}
 		}catch(exeption $e){
 			echo $e;
 		}
 	}
+	/**
+	 * アクセス数を取得
+	 *
+	 * @return Integer
+	 */
 	function Get_int_Count() {
 		return $this -> int_count;
 	}
